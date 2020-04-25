e3ba754daef781e0ca09e62d19b14209467ffc8c
Dhanunjay <dhanunjaysagaraskani@gmail.com>
2020-04-25 21:02:19 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express=require('express'),
+      path=require('path'),
+      morgan=require('morgan'),
+      mysql=require('mysql'),
+      myConnection=require('express-myconnection');
+
+      
+      
+
+const app=express();
+
+//importing routes
+const userRoutes=require('./src/routes/users');
+
+
+//settings
+
+app.set('views',path.join(__dirname,'src/views'));
+app.set('view engine','ejs');
+
+
+//morgan for http requestlogger
+app.use(morgan('dev'));
+app.use(myConnection(mysql,{
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database:'users_info'
+},'single'));
+
+app.use(express.urlencoded({extended:false}));
+
+//routes
+app.use('/',userRoutes);
+//static files
+
+app.use(express.static(path.join(__dirname,'public')));
+
+
+
+const port=process.env.PORT||3000;
+app.listen(port,()=>{console.log(`App listening on port ${port}...`)});
